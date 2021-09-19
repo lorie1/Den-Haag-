0e56daf65e1793fd501b2ec502281c1894361083
lorie1 <87571590+lorie1@users.noreply.github.com>
2021-09-19 15:40:42 +0200

    fc

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     var serverEmbed = new discord.MessageEmbed()
             .setDescription("Hallo, wij zijn Den Haag")
             .setColor("#0b03fc")
-            .addField("Server naam: ", serverName)
+            .addField("Server naam: DenHaag {BETA}")
             .addField("Je bent deze server gejoind op", message.member.joinedAt)
             .addField("Totaal memebers", message.guild.memberCount)
             .setTimestamp();
