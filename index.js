4e9174757d8461c2e7b43aa257bf31b15df85ef3
lorie1 <87571590+lorie1@users.noreply.github.com>
2021-09-19 13:55:50 +0200

    ef

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const botConfig = require("./botconfig.json");
 const fs = require("fs");
 const { SlowBuffer } = require("buffer");
 
-bot.login(process.env.token)
 
 const bot = new discord.Client();
 bot.commands = new discord.Collection();
@@ -63,4 +62,54 @@ return guildMemberAdd.send(DMembed);
 
 
 
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} doet het!.`);
+
+    bot.user.setActivity(`Bot maker: Lorie#1093`, { type: `LISTENING` });
+
+});
+
+
+bot.on("message", async message => {
+
+    if (message.author.bot) return;
+
+    if (message.channel.type === "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+    
+    var arguments = messageArray.slice(1);
+    
+    var commands = bot.commands.get(command.slice(prefix.length));
+
+    if (commands) commands.run(bot, message, arguments);
+
+
+
+
+});
+
+bot.login(process.env.token);
