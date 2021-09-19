bd8a2e3157b2a48b5a97ccff004b4ba7a8ca3b55
lorie1 <87571590+lorie1@users.noreply.github.com>
2021-09-19 15:46:36 +0200

    efs

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,8 +6,8 @@ module.exports.run = async (client, message, args) => {
             .setDescription("Hallo, wij zijn Den Haag")
             .setColor("#0b03fc")
             .setDescription("**Server naam:** DenHaag {BETA}")
-            .addField("Je bent deze server gejoind op", message.member.joinedAt)
-            .addField("Totaal memebers", message.guild.memberCount)
+            .addField("Je bent deze server gejoind op", message.member.joinedAt, true)
+            .addField("Totaal memebers", message.guild.memberCount, true)
             .setTimestamp();
 
         return message.channel.send(serverEmbed);
